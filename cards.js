6f817599a0d8550f755fc8caaa96fa5f5b9ff9a4
JayS-v <viccsey@gmail.com>
2023-09-18 19:40:38 +0200

    likeshandler() added

--- cards.js
+++ cards.js
@@ -3,53 +3,61 @@ const cardsInitial = [
         dataFilter: 'red',
         imageSource: 'images/red0.png',
         link: 'https://en.wikipedia.org/wiki/Red_fox',
+        likes: 2547,
     },
     {
         dataFilter: 'pet',
         imageSource: 'images/pet0.png',
         link: 'https://en.wikipedia.org/wiki/Domesticated_silver_fox',
+        likes: 9764,
     },
     {
         dataFilter: 'pet',
         imageSource: 'images/pet1.png',
         link: 'https://en.wikipedia.org/wiki/Domesticated_silver_fox',
+        likes: 1000,
     },
     {
         dataFilter: 'kit',
         imageSource: 'images/kit0.png',
         link: 'https://en.wikipedia.org/wiki/Kit_fox',
+        likes: 1287,
     },
     {
         dataFilter: 'arctic',
         imageSource: 'images/arctic0.png',
         link: 'https://en.wikipedia.org/wiki/Arctic_fox',
+        likes: 4659,
     },
     {
         dataFilter: 'fennec',
         imageSource: 'images/fennec0.png',
         link: 'https://en.wikipedia.org/wiki/Fennec_fox',
+        likes: 9897,
     },
     {
         dataFilter: 'kit',
         imageSource: 'images/kit1.png',
         link: 'https://en.wikipedia.org/wiki/Kit_fox',
+        likes: 37,
     },
     {
         dataFilter: 'red',
         imageSource: 'images/red1.png',
         link: 'https://en.wikipedia.org/wiki/Red_fox',
+        likes: 1100,
     },
 ]
 
 //add ID's 
-export const cards = cardsInitial.map((item, index) => ({ ...item, id: index + 1 }))
+export const cards = cardsInitial.map((item, index) => ({ ...item, id: 'fox' + Number(index + 1)}))
 
 export const cardContent = (arrayItem) => {
     return `
-    <section class="content-block data-filter="${arrayItem.dataFilter}">
+    <section class="content-block" id="${arrayItem.id}" data-filter="${arrayItem.dataFilter}">
         <div class="content-block__body">
             <h2 class="content-block__title">${arrayItem.dataFilter.charAt(0).toUpperCase() + arrayItem.dataFilter.slice(1)} smile fox</h2>
-            <span class="likes"></span>
+            <span class="likes">${arrayItem.likes}</span>
             <p class="content-block__text">Lorem ipsum dolor sit amet, eu mei posse possim atomorum, vix cu fabellas assueverit. Vel ad option partiendo maiestatis, nec affert appetere.
             </p>
         </div>
@@ -59,3 +67,4 @@ export const cardContent = (arrayItem) => {
     </section>
     `
 }
+
--- main.js
+++ main.js
@@ -4,22 +4,50 @@ const pageContent = document.querySelector('.page-content');
 const menu = document.querySelector('.menu');
 const menuLinks = document.querySelectorAll('.menu__link');
 
+let filteredArray = [...cards];
+
 const render = (DOMElement, array, arrayItemContent) => DOMElement.innerHTML = array.map(arrayItemContent).join("");
 
-const showInitialData = () => render(pageContent, cards, cardContent);
+const showInitialData = () => render(pageContent, filteredArray, cardContent);
 
-window.addEventListener('DOMContentLoaded', async (event) => showInitialData());
+
+window.addEventListener('DOMContentLoaded', async (event) => {
+    showInitialData();
+    likesHandler(filteredArray);
+});
+
+
+const likesHandler = (array) => {
+    pageContent.addEventListener('click', function (event) {
+        if (event.target.className === 'likes') {
+            const clickedCardID = event.target.closest('section').id;
+    
+            let newLikeNumber = Number(event.target.innerHTML) + 1;
+            console.log(newLikeNumber);     //new like number 
+    
+            array.map(card => {            
+                if (card.id === clickedCardID) {
+                    card.likes = newLikeNumber;
+                }
+                return card;
+            });
+
+            render(pageContent, filteredArray, cardContent);
+        } 
+    });
+}
 
 menu.onclick = event => {
     const clickedMenuItem = event.target.dataset.filter;  
 
     if (event.target.tagName == 'A') {
         if (!event.target.classList.contains('selected') && cards.some(card => card.dataFilter === clickedMenuItem)){
-            const newArray = cards.filter(card => card.dataFilter === clickedMenuItem);
-            render(pageContent, newArray, cardContent);
-            createPopUp(event, clickedMenuItem);
+            filteredArray = cards.filter(card => card.dataFilter === clickedMenuItem);
+            render(pageContent, filteredArray, cardContent);
+            popUpLinks(event, clickedMenuItem);
         } else if (event.target.classList.contains('selected')) {
-           showInitialData();
+            filteredArray = [...cards];
+            showInitialData();
         }        
         else {
             console.log(`card is ${clickedMenuItem}`);
@@ -29,8 +57,8 @@ menu.onclick = event => {
     } 
 };
 
-const createPopUp = (event, clickedMenuItem) => {
-    const clickedCardITem = cards.find(card => card.dataFilter == clickedMenuItem);
+const popUpLinks = (event, clickedMenuItem) => {
+    const clickedCardItem = cards.find(card => card.dataFilter == clickedMenuItem);
     let popUp;
 
     if (document.querySelector('dialog')){
@@ -38,7 +66,7 @@ const createPopUp = (event, clickedMenuItem) => {
     }
 
     popUp = document.createElement('dialog');
-    const popUpLink = `Open <a href="${clickedCardITem.link}" target="_blank">${clickedCardITem.link}</a>`
+    const popUpLink = `Open <a href="${clickedCardItem.link}" target="_blank">${clickedCardItem.link}</a>`
     popUp.insertAdjacentHTML('beforeend', popUpLink);
     event.target.appendChild(popUp);
     popUp.show();
@@ -50,3 +78,13 @@ const createPopUp = (event, clickedMenuItem) => {
         }
     });
 }
+
+
+
+
+
+
+
+
+
+
